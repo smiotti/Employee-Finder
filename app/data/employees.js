62d7e646cfb20592db962e6697f4828250eec711
Miotti <210061319@LOGON.DS.GE.COM>
2018-12-16 22:52:08 -0500

    updates to html JS Readme files

--- app/data/employees.js
+++ app/data/employees.js
@@ -1,76 +1,27 @@
-// This will hold all of the employee survey data (objects)
-// Building an intial employee data set for use in employee match logic
+// This will hold all of the employee survey data (as objects)
+// Building an intial employee data set for use in employee closest match logic
 
-module.exports = [
-    {
-        "name": "Clark Kent",
+const employees = [
+    {   "name": "Clark Kent",
         "photo": "https://imagesyoulike.com/images/j/32x24/j1960.jpg",
-        "scores": [
-            5,
-            5,
-            1,
-            1,
-            5,
-            2,
-            5,
-            5,
-            2,
-            5
-        ]
+        "scores": [5, 5, 1, 1, 5, 2, 5, 5, 2, 5]
     },
 
-    {
-        "name": "Bruce Wayne",
+    {   "name": "Bruce Wayne",
         "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHGILhWcsPLpYHStxuAWrt6Mmhust9OysTA7BcRhSm2pqyWOgVgw",
-        "scores": [
-            4,
-            2,
-            4,
-            1,
-            3,
-            4,
-            4,
-            4,
-            5,
-            2
-        ]
+        "scores": [4, 2, 4, 1, 3, 4, 4, 4, 5, 2]
     },
 
-    {
-        "name": "Tony Stark",
+    {   "name": "Tony Stark",
         "photo": "https://cdn3.movieweb.com/i/article/OybF9mMBr9EkmGz94QCKPB2in4smsG/798:50/Avengers-4-Tony-Stark-Iron-Man-Dead-Writer.jpg",
-        "scores": [
-            2,
-            1,
-            5,
-            5,
-            4,
-            5,
-            4,
-            4,
-            2,
-            1
-        ]
+        "scores": [2, 1, 5, 5, 4, 5, 4, 4, 2, 1]
     },
 
-    {
-        "name": "Natasha Romanova",
+    {   "name": "Natasha Romanova",
         "photo": "https://30c57e491d34574ac3c4-d69fd22d5bffeab44970d3ac75e05830.ssl.cf2.rackcdn.com/production/user/profile/296008/1521247791.jpg",
-        "scores": [
-            5,
-            3,
-            2,
-            4,
-            2,
-            2,
-            5,
-            4,
-            1,
-            4
-        ]
+        "scores": [5, 3, 2, 4, 2, 2, 5, 4, 1, 4]
     }
 ];
 
-
-// Exporting employee array. 
-// module.exports = employees;
+// Exporting employee array to make it accessible to other files using require. 
+module.exports = employees;
--- app/routes/api_routes.js
+++ app/routes/api_routes.js
@@ -7,14 +7,25 @@
 // results. This route will also be used to handle the compatibility logic.
 
 
+// 6. Determine the user's most compatible employee using the following as a guide:
+
+//    * Convert each user's results into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`).
+//    * With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the `totalDifference`.
+//      * Example: 
+//        * User 1: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`
+//        * User 2: `[3, 2, 6, 4, 5, 1, 2, 5, 4, 1]`
+//        * Total Difference: **2 + 1 + 2 =** **_5_**
+//    * Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both `5-3` and `3-5` as `2`, and so on. 
+//    * The closest match will be the user with the least amount of difference.
 
 
 // Importing employee data for use in routes 
 const employees = require('../data/employees.js');
 
 // Exporting routes
-module.exports = function(app){
+module.exports = function (app) {
   
+
   // Display JSON for all employees.
   app.get('/api/employees', function(req, res) {
     res.json(employees);
@@ -22,107 +33,89 @@ module.exports = function(app){
     console.log(req.method + " request");
     console.log(req.body);
   });
-  
-  
-  // Handles incoming survey results and adds to the employee data. This route also used to handle the compatibility logic.
-  app.post('/api/employees', function(req, res) {
-    employees.push(req.body);
-    // res.json(req.body);
-    
-    res.send('Survey form submitted successfuly!')
+
+
+  // Handles incoming survey results and adds to the employee data. 
+  // This route also used to handle the compatibility logic.
+  app.post('/api/employees', function (req, res) {
     console.log(req.method + " request");
     console.log(req.body);
-  
-    //   // Respond with success of the delete operation (true or false)
-                  //   return res.json({ success: success })
-    console.log("Form submit successful!");
-    
-        // Test code to remove
-        // app.post('/', function(req, res) {
-        //   console.log(req.body);
-        //   res.end();
-        // });
+   
 
+    // Defining object that will be used to hold the closed matched employee, as the employee list is looped through. 
+    const closestMatch = {
+      name: '',
+      photo: '',
+      employeeDifference: Infinity
+    };
 
-    // Here we take the result of the user's survey POST and parse it.
+    // Parse result of the user's survey POST request
     const userData = req.body;
-    // const userScores = userData.scores;
+    const userScores = userData.scores;
+    // console.log('userData' + userData);
+    console.log('*** userScore(s) before parseint ****' + req.body.scores);
+
+
+    // variables used to calculate the absolute difference between the user's scores and the scores of
+    // each employee in the data file.  
+    let absoluteDifference
+    let totalDifference
+
+    // Looping through each of the employee possibilities in the employees data.
+    for (let i = 0; i < employees.length; i++) {
+      const currentEmployee = employees[i];
+      absoluteDifference = 0;
+      totalDifference = 0;
+
+      // Looping through all the scores for each employee
+      for (let j = 0; j < currentEmployee.scores.length; j++) {
+        const currentEmployeeScore = parseInt(currentEmployee.scores[j]);
+        const currentUserScore = parseInt(userScores[j]);
+    
+
+        // First calculate the absolute difference between the user and employee scores
+        // Then update the total (or sum) of the differences.  Logging calculations to console
+        absoluteDifference = Math.abs((currentUserScore) - (currentEmployeeScore));
+        totalDifference += absoluteDifference; 
+        console.log(`${userData.name} Score: ${currentUserScore}`);
+        console.log(`${currentEmployee.name} Score: ${currentEmployeeScore}`);
+        console.log(`Abs Difference = ${parseInt(currentUserScore)} - ${parseInt(currentEmployeeScore)} = ${absoluteDifference}`)
+        console.log(`${userData.name} Total Difference: ${totalDifference}`);
+      }
+
+      // If the sum of differences is less then the differences of the current 'closest match'
+      if (totalDifference <= closestMatch.employeeDifference) {
+        // Reset the closestMatch to be the new employee.
+        closestMatch.name = currentEmployee.name;
+        closestMatch.photo = currentEmployee.photo;
+        closestMatch.employeeDifference = totalDifference;
+      }
+    }
+
+
+    // Looping through the User's scores and converting to integers before witing out to Employee file
+    for (let i = 0; i < userData.scores.length; i++) {
+      req.body.scores[i] = parseInt(userData.scores[i]);
+      // console.log('*** updating user score to integer ****' + req.body.scores);
+    }
+
+
+    // Save the user's data to the employees data file. 
+    employees.push(userData);
+    console.log(`${userData.name}'s closest match is ${closestMatch.name}, with a difference of ${totalDifference}.`);
+    console.log("*** Form submit successful! ***");
 
     
-    // Finally save the user's data to the employees data (this has to happen AFTER the check. otherwise,
-    // the employees data will always return that the user is the user's best match).
-    // employees.push(userData);
-    
-
-    
-
-
+    // Return JSON with the user's closestMatch. This will be used by the HTML in the next page
+    res.json(closestMatch);
   });
+};
 
 
 
+// res.send('Survey form submitted successfuly!')
 
 
-
-                  
-                  // // Displays a single employee, or shows 'No employee found'
-                  // app.get('/api/employees/:employee', function(req, res) {
-
-                  //   // Grab the selected parameter
-                  //   const chosen = req.params.employee;
-                  //   console.log(chosen);
-
-                  //   // Filter to show only the selected employee
-                  //   for (let i = 0; i < employees.length; i++) {
-                  //     if (chosen === employees[i].routeName) {
-                  //       return res.json(employees[i]);
-                  //     }
-                  //   }
-
-                  //   // Otherwise respond with false
-                  //   return res.json(false);
-                  // });
-
-                  // // Finds the requested employee and replaces it one provided in the request body
-                  // app.put('/api/employees/:employee', function(req, res) {
-
-                  //   // Grab the selected parameter
-                  //   const chosen = req.params.employee;
-                  //   let found = false;
-
-                  //   // Find the specified employee and replace the object with the one provided
-                  //   for (let i = 0; i < employees.length; i++) {
-                  //     if (chosen === employees[i].routeName) {
-                  //       employees.splice(i, 1, req.body);
-                  //       found = true;
-                  //     }
-                  //   }
-
-                  //   if(found){
-
-                  //     // If our requested employee was found, respond with the updated object
-                  //     return res.json(req.body)
-                  //   } 
-
-                  //   // Otherwise, respond with false
-                  //   return res.json(false)
-                  // });
-
-                  // // Finds the requested employee and deletes it from the collection
-                  // app.delete('/api/employees/:employee', function(req, res) {
-                  //   // Grab the selected parameter
-                  //   const chosen = req.params.employee;
-                  //   let success = false;
-
-                  //   // Find the specified employee and remove it from the collection
-                  //   for (let i = 0; i < employees.length; i++) {
-                  //     if (chosen === employees[i].routeName) {
-                  //       employees.splice(i, 1);
-                  //       success = true;
-                  //     }
-                  //   }
-
-                  //   // Respond with success of the delete operation (true or false)
-                  //   return res.json({ success: success })
-                  // });
-}
+// Respond with success of the delete operation (true or false)
+      // res.json({ success: success })
+    
