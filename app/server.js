93882345058bb809cdd87aabe36cec9326cb8dfe
Miotti <210061319@LOGON.DS.GE.COM>
2018-12-18 11:00:50 -0500

    JS file gitignore

--- app/server.js
+++ app/server.js
@@ -8,10 +8,11 @@ const app = express();
 // setting up the port number
 // const PORT = 8080;
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
-}
+var PORT = process.env.PORT || 8000;
+// let port = process.env.PORT;
+// if (port == null || port == "") {
+//   port = 8000;
+// }
 // app.listen(port);
 
 // Sets up the Express app to handle data parsing.
@@ -29,8 +30,8 @@ require('./routes/html_routes.js')(app);
 
 
 // *** Listener ***
-app.listen(port, function() {
-  console.log(`App listening on PORT http://localhost:${port}`);
+app.listen(PORT, function() {
+  console.log(`App listening on PORT http://localhost:${PORT}`);
 });
 
 
